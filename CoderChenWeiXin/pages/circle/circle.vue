ebee3498d81a1fc5580a5ff935ee4430327e0f2b
ChenJiPan <cll7781@163.com>
2020-04-07 09:43:59 +0800

    新建微信小程序

---
+++ CoderChenWeiXin/pages/circle/circle.vue
@@ -0,0 +1,39 @@
+<template>
+	<view class="box">
+		<text @click="ppppppp">{{title}}</text>
+		克拉克森的历史
+	</view>
+</template>
+
+<script>
+	export default{
+		data(){
+			return {
+				title:"张三哥"
+			}
+		},
+		methods:{
+			ppppppp(){
+				this.title = "李四"
+			}
+		},
+		onLoad() {
+			console.log("视图创建完成")
+		},
+		onShow() {
+			console.log("视图出现")
+		},
+		onHide() {
+			console.log("视图消失")
+		},
+		mounted() {
+			console.log("更好地哈四大皆空");
+		},
+		
+		
+		
+	}
+</script>
+
+<style>
+</style>
